c60010cb76272a1852d8009de63c9d5486e541f0
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-02-21 18:15:38 -0300

    refactor: aplicando modelo de projeto para organização

---
+++ src/http/controllers/users/register.controller.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/libs/prisma.js"
+import { z } from "zod"
+import type { FastifyRequest, FastifyReply } from "fastify"
+
+
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+    const registerBodySchema = z.object({
+        name: z.string().trim().min(1).max(100),
+        email: z.email().max(100),
+        password: z.string().min(8).max(100),
+        photo: z.string(),
+    })
+
+    const { name, email, password, photo} = registerBodySchema.parse(request.body)
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password,
+            photo
+        }
+    })
+
+    return reply.status(201).send(user)
+}
