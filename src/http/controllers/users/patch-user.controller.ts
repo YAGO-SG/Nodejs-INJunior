7748506732114cac2a35d070ceb8e43b964e7a54
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-02-24 18:36:17 -0300

    feat: implementando requisição de atualização de usuário

---
+++ src/http/controllers/users/patch-user.controller.ts
@@ -0,0 +1,35 @@
+import { prisma } from "@/libs/prisma.js";
+import type { FastifyRequest, FastifyReply } from "fastify";
+import { z } from 'zod'
+
+export async function patchUser(req: FastifyRequest, rep: FastifyReply) {
+    try {
+        const { id } = req.params;
+
+        const patchBodySchema = z.object({
+            name: z.string().trim().min(1).max(100),
+            email: z.email().max(100),
+            password: z.string().min(8).max(100),
+            photo: z.string(),
+        })
+
+        const { name, email, password, photo} = patchBodySchema.parse(req.body)
+
+        const updateUser = await prisma.user.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                name,
+                email,
+                password,
+                photo,
+            }
+        })
+        
+        rep.status(200).send(updateUser)
+
+    } catch (error) {
+        rep.status(200).send(error)
+    }
+}
--- src/http/controllers/users/user.routes.ts
+++ src/http/controllers/users/user.routes.ts
@@ -2,9 +2,11 @@ import type { FastifyInstance } from "fastify";
 import { register } from "./register.controller.js";
 import { listUsers } from "./listUsers.controller.js";
 import { deleteUser } from "./delete-user.controller.js";
+import { patchUser } from "./patch-user.controller.js";
 
 export async function usersRoutes(app: FastifyInstance) {
-    app.post('/', register),
-    app.get('/list', listUsers)
-    app.delete('/delete/:id', deleteUser)
+    app.post('/', register);
+    app.get('/list', listUsers);
+    app.delete('/delete/:id', deleteUser);
+    app.patch('/update/:id', patchUser);
 }
